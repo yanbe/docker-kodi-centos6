001dc97cc3868e7e174e8156effa11fe85625178
yanbe <y.yanbe@gmail.com>
2018-10-24 09:24:19 +0900

    tail both xorg.0.log and kodi.log

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+rm -f /var/run/messagebus.pid
 service messagebus start
 service haldaemon start
 service acpid start
 startx &
 sleep 1
-tail -n 100 -f /var/log/Xorg.0.log
+tail -n +1 -f /var/log/Xorg.0.log /root/.kodi/temp/kodi.log
 service acpid stop
 service haldaemon stop
 service messagebus stop
